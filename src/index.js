a1d40d19e5c194647fdab49b5d1e56c736b82106
junrezapico <junre@vananaz.com>
2022-12-13 15:19:30 +0800

    added working 6k steps

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import * as tf from "@tensorflow/tfjs";
 import "./styles.css";
 tf.setBackend("webgl");
 
+// const threshold = 0.35;
 const threshold = 0.75;
 
 async function load_model() {
@@ -12,20 +13,20 @@ async function load_model() {
   // It's possible to load the model locally or from a repo
   // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
   // const model = await loadGraphModel("http://localhost:51196/model.json");
-  
-  // const model = await tf.loadGraphModel("http://127.0.0.1:8080/model.json");
-  
+
+  const model = await tf.loadGraphModel("http://127.0.0.1:8080/model.json");
+
   // const model = Object.freeze(await tf.loadGraphModel("http://127.0.0.1:8080/model.json"));
   // console.log('harr');
   // const model = await loadGraphModel("http://127.0.0.1:3000/model/kangaroo-detector/model.json");
-  // const model = await loadGraphModel(
+  // const model = await tf.loadGraphModel(
   //   "https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/kangaroo-detector/model.json"
   // );
 
   // const model = await tf.loadGraphModel(
   //   "https://raw.githubusercontent.com/itsmitchyyy/itsmitchyyy.github.io/main/ar-playground/another_model/model.json"
   // );
-  // const model = await tf.loadGraphModel('https://2595-120-28-226-166.ap.ngrok.io');
+  // const model = await tf.loadGraphModel('https://raw.githubusercontent.com/junrezapico/hello-world-tensorflow/main/models/rubick-2k-steps/model.json');
   return model;
 }
 
@@ -88,7 +89,7 @@ class App extends React.Component {
 
       Promise.all([
         modelPromise,
-        webCamPromise
+        webCamPromise,
         // new Promise(resolve => resolve(true))
       ])
         .then((values) => {
@@ -111,7 +112,12 @@ class App extends React.Component {
     tf.engine().startScope();
     // console.log('oing ingg', this.process_input(video));
     model.executeAsync(this.process_input(video)).then((predictions) => {
-      console.log("amng predictions", predictions);
+      // console.log("amng predictions", predictions);
+
+      // for (let i = 0; i < predictions.length; i++) {
+      //   console.log('mehehe',i,predictions[i].dataSync());
+      //   console.log('mehehe',i,predictions[i].arraySync());
+      // }
       this.renderPredictions(predictions, video);
       requestAnimationFrame(() => {
         this.detectFrame(video, model);
@@ -129,12 +135,43 @@ class App extends React.Component {
   buildDetectedObjects(scores, threshold, boxes, classes, classesDir) {
     const detectionObjects = [];
     var video_frame = document.getElementById("frame");
-    // /*
-    console.log('scoresss', {scores, threshold});
-
-    scores[0].forEach((score, i) => {
+    // console.log("hohoy1", scores, threshold);
+    scores[0].forEach((_score, i) => {
+      // scores.forEach((_score, i) => {
+      // const score = _score;
+      // const score = _score[1];
+      // const score = _score[0];
+      // muwork sa akoa
+      const score = (_score[1]);
+      // const score = (_score[0]+_score[1])/2;
+      console.log("hohoy", scores, score, _score, threshold, boxes);
       if (score > threshold) {
+        console.log("hohoy pasok", score, _score);
         const bbox = [];
+        // // muwork sa akng mitch
+        // const minY = boxes[0][i][0] * video_frame.offsetHeight;
+        // const minX = boxes[0][i][1] * video_frame.offsetWidth;
+        // const maxY = boxes[0][i][2] * video_frame.offsetHeight;
+        // const maxX = boxes[0][i][3] * video_frame.offsetWidth;
+        // // const minY = 0;
+        // // const minX = 0;
+        // // const maxY = 0;
+        // // const maxX = 0;
+        // bbox[0] = minX;
+        // bbox[1] = minY;
+        // bbox[2] = maxX - minX;
+        // bbox[3] = maxY - minY;
+
+        // // muwork para sa akoa 2krubicks
+        // const minY = boxes[i * 4] * video_frame.offsetHeight;
+        // const minX = boxes[i * 4 + 1] * video_frame.offsetWidth;
+        // const maxY = boxes[i * 4 + 2] * video_frame.offsetHeight;
+        // const maxX = boxes[i * 4 + 3] * video_frame.offsetWidth;
+        // bbox[0] = minX;
+        // bbox[1] = minY;
+        // bbox[2] = maxX - minX;
+        // bbox[3] = maxY - minY;
+        // muwork para sa akoa 6krubicks
         const minY = boxes[0][i][0] * video_frame.offsetHeight;
         const minX = boxes[0][i][1] * video_frame.offsetWidth;
         const maxY = boxes[0][i][2] * video_frame.offsetHeight;
@@ -143,17 +180,14 @@ class App extends React.Component {
         bbox[1] = minY;
         bbox[2] = maxX - minX;
         bbox[3] = maxY - minY;
-        console.log('badada', bbox);
         detectionObjects.push({
           class: classes[i],
-          // label: classesDir[classes[i]].name,
-          label: 'lorem',
+          label: `rubicks - ${score} = ${_score[0]} + ${_score[1]}`,
           score: score.toFixed(4),
           bbox: bbox,
         });
       }
     });
-    // */
     return detectionObjects;
   }
 
@@ -167,8 +201,20 @@ class App extends React.Component {
     ctx.textBaseline = "top";
 
     //Getting predictions
-    const boxes = predictions[4].arraySync();
-    const scores = predictions[5].arraySync();
+    // // muwork sa kang mitch
+    // const boxes = predictions[4].arraySync();
+    // const scores = predictions[5].arraySync();
+    // const classes = predictions[6].dataSync();
+    // muwork sa akoang 2k
+    // const boxes = predictions[0].dataSync();
+    // const scores = predictions[1].arraySync();
+    // const classes = predictions[2].dataSync();
+    // const boxes = predictions[1].dataSync();
+    // const scores = predictions[0].dataSync();
+    // const classes = predictions[2].dataSync();
+
+    const boxes = predictions[2].arraySync();
+    const scores = predictions[4].arraySync();
     const classes = predictions[6].dataSync();
     const detections = this.buildDetectedObjects(
       scores,
@@ -210,6 +256,7 @@ class App extends React.Component {
       const y = item["bbox"][1];
       const width = item["bbox"][2];
       const height = item["bbox"][3];
+      const label = item["label"];
 
       // Draw the bounding box.
       ctx.strokeStyle = "#00FFFF";
@@ -224,7 +271,7 @@ class App extends React.Component {
       const textHeight = parseInt(font, 10); // base 10
       ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
       ctx.fillStyle = "#000000";
-      ctx.fillText('lorem ipsum', x, y);
+      ctx.fillText(label, x, y);
 
       // -5 from the left most, 5 to the right most, 0 is center
       // xPos: 0,
@@ -235,10 +282,10 @@ class App extends React.Component {
       // const newX = ((x + width / 2) / window.innerWidth) * 10 - 5;
       // const newY = 5 - ((y + height / 2) / window.innerHeight) * 5;
       // formula if for mitch
-      const newX = ((x) / window.innerWidth) * 10 - 5;
+      const newX = (x / window.innerWidth) * 10 - 5;
       // const newY = 5 - ((y) / window.innerHeight) * 5;
       const newY = 2;
-      console.log('mao ni sha', {newX, newY});
+      // console.log("mao ni sha", detections);
 
       document.querySelector("a-entity").setAttribute("position", {
         x: newX,
